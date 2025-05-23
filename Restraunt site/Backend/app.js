c934a47ada0cc9d899e10b2aa8105d419c9747c6
sourav-xe <sourav20kumar02@gmail.com>
2025-05-23 13:30:04 +0530

    debugging done

--- Restraunt site/Backend/app.js
+++ Restraunt site/Backend/app.js
@@ -16,7 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/v1", reservationRoutes);
+app.use("/api/v1/reservation", reservationRoutes);
 
 dbconnection();
 
